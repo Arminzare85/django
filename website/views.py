50707594165b51b3fff3dcf73e86f377b5fbb6e9
Arminzare85 <shamino85xrmxn@gmail.com>
2026-07-17 17:58:01 +0330

    merging my template with my app

--- website/views.py
+++ website/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def home_views(request):
-    return render(request ,'website/home.html')
+    return render(request ,'website/index.html')
 
 def about_views(request):
     return render(request ,'website/about.html')
