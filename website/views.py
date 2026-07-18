76b2e2f6bf5a9747a987eb4118f8d3d059f4dd9a
Arminzare85 <shamino85xrmxn@gmail.com>
2026-07-18 15:03:53 +0330

    dynamic items to use from database (testing !)

--- website/views.py
+++ website/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 
 def home_views(request):
-    return render(request ,'website/index.html')
+    context={'name':'Armin Zare' , 'text' :'I am armin zare  , im trying to learn Django , its hard but i can do this as always'}
+    return render(request ,'website/index.html' , context)
 
 def about_views(request):
     return render(request ,'website/about.html')
