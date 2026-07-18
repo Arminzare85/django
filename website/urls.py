f8ca975bcd4fcf0af9f444893d85406565ceaa45
Arminzare85 <shamino85xrmxn@gmail.com>
2026-07-18 14:20:04 +0330

    dynamic urls in header !!!

--- website/urls.py
+++ website/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path , include
 from website.views import *
 
+app_name =  'website'
+
 urlpatterns = [
     
-    path("",home_views),
-    path("about",about_views),
-    path("contact",contact_views)   
+    path("",home_views,name="index"),
+    path("about",about_views,name="about"),
+    path("contact",contact_views,name="contact")   
 ]
