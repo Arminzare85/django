a43c1aca08b578bb5eb1bdef7b13687f29e820ff
Arminzare85 <shamino85xrmxn@gmail.com>
2026-07-21 16:25:50 +0330

    posting posts with true published time and showing view counted

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse,JsonResponse
 from blog.models import Post
+from django.utils import timezone
 
 def blog_view(request):
     posts = Post.objects.filter(status =1)
@@ -11,8 +12,15 @@ def blog_view(request):
 def blog_single(request):
     return render(request ,'blog/blog-single.html')
 def test(request , pid):
-    post= Post.objects.get(id = pid)
-    context = {'post': post}
+    posts = get_object_or_404(
+        Post,
+        id=pid,
+        published_date__lte=timezone.now()
+    )
+    posts.counted_view += 1
+    posts.save()
+
+    context = {'posts': posts}
     return render(request , 'test.html' , context)
 
 
