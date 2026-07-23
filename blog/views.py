810d6abce924100c92054288b7a3682dfc297bca
Arminzare85 <shamino85xrmxn@gmail.com>
2026-07-23 14:02:57 +0330

    Category added
    category with ManyToMany logic added and dynamic img's added
    using prev post and next post on blog-single with dynamic url

--- blog/views.py
+++ blog/views.py
@@ -10,8 +10,21 @@ def blog_view(request):
     return render(request ,'blog/blog-home.html',context)
 
 def blog_single(request , pid):
+    posts = list(Post.objects.filter(status =1))
     post = get_object_or_404( Post , id = pid)
-    context = {'post': post}
+    post_index = posts.index(post)
+    if post_index>0:
+        previous_post = posts[post_index - 1]
+    else:
+        previous_post = None
+    if post_index < len(posts) - 1:
+       next_post = posts[post_index + 1]
+    else:
+        next_post = None
+    context = {'post': post,
+               'previous_post': previous_post ,
+               'next_post': next_post 
+               }
     return render(request ,'blog/blog-single.html' , context)
 def test(request,pid ):
     posts = get_object_or_404( Post ,id = pid)
