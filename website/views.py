e227a1212fd5ee0e5e96e3d4a0c3b945db1bdae9
Arminzare85 <shamino85xrmxn@gmail.com>
2026-07-29 13:25:20 +0330

    contact form created

--- website/views.py
+++ website/views.py
@@ -1,16 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse,JsonResponse
-from blog.models import Post
+from website.forms import ContactForm
+from django.contrib import messages
 
 def home_views(request): 
-    posts = Post.objects.filter(status=True)
-    context={'name':'Armin Zare' , 'text' :'I am armin zare  , im trying to learn Django , its hard but i can do this as always',"posts": posts}
+    
+    context={'name':'Armin Zare' , 'text' :'I am armin zare  , im trying to learn Django , its hard but i can do this as always'}
     return render(request ,'website/index.html' , context)
 
 def about_views(request):
     return render(request ,'website/about.html')
 
-def contact_views(request):
-    return render(request ,'website/contact.html')
 
+
+def contact_views(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.name = "unknown"
+            form.save()
+            messages.success(request, "successfully posted")
+            return redirect("website:contact")
+
+    else:
+        form = ContactForm()
+
+    return render(request, "website/contact.html", {"form": form})
+    
 
